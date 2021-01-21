ebaea55315de620bfc66c6f5e8dedd8bde738764
tungdt <tungdt@funtap.vn>
2021-01-21 17:02:40 +0700

    first

--- number_chart/lib/model/user_model.dart
+++ number_chart/lib/model/user_model.dart
@@ -1,10 +1,46 @@
-class UserModel{
+import 'package:flutter/foundation.dart';
 
-  String _phone;
-  String _userName;
-  Map<String,int> _quantityTypeGame;
-  int _totalBuy;
-  int _totalReward;
+class UserModel with ChangeNotifier {
+  String _userPhone = '0964348059';
+  String _userName = 'Tung';
+  Map<String, int> _quantityTypeGame = {'Lo':50,'De':10};
+  double _totalBuy = 100000;
+  double _totalReward = 0;
+
+  String get userPhone => _userPhone;
+
+  String get userName => _userName;
+
+  Map<String, int> get quantityTypeGame => _quantityTypeGame;
+
+  double get totalBuy => _totalBuy;
+
+  double get totalReward => _totalReward;
+
+  set userPhone(String userPhone) {
+    _userPhone = userPhone;
+    notifyListeners();
+  }
+
+  set userName(String userName) {
+    _userName = userName;
+    notifyListeners();
+  }
+
+  set quantityTypeGame(Map<String, int> quantityTypeGame) {
+    _quantityTypeGame = quantityTypeGame;
+    notifyListeners();
+  }
+
+  set totalBuy(double totalBuy) {
+    _totalBuy = totalBuy;
+    notifyListeners();
+  }
+
+  set totalReward(double totalReward) {
+    _totalReward = totalReward;
+    notifyListeners();
+  }
 
 
 }
--- number_chart/lib/ui/home_screen.dart
+++ number_chart/lib/ui/home_screen.dart
@@ -1,10 +1,7 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:number_chart/model/user_model.dart';
 import 'package:number_chart/utils/util.dart';
-import 'package:syncfusion_officechart/officechart.dart';
-import 'package:syncfusion_flutter_xlsio/xlsio.dart' hide Column, Alignment;
-import 'package:path_provider/path_provider.dart' as path_provider;
-import 'package:open_file/open_file.dart' as open_file;
+import 'package:provider/provider.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -14,64 +11,24 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(Strings.titleApp),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            FlatButton(
-              child: const Text(
-                'Generate Excel',
-                style: TextStyle(color: Colors.white),
-              ),
-              color: Colors.blue,
-              onPressed: generateOfficeChart,
-            ),
-          ],
+    var userModel = Provider.of<UserModel>(context);
+    return ChangeNotifierProvider(
+      create: (context) => UserModel(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(userModel.userPhone),
+        ),
+        body: Center(
+          child: Container(),
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.blueAccent,
+          onPressed: (){
+
+          },
+          child: Icon(Icons.change_history),
         ),
       ),
     );
-  }
-  Future<void> generateOfficeChart() async {
-// Create a new Excel document.
-    final Workbook workbook = Workbook();
-// Accessing worksheet via index.
-    final Worksheet sheet = workbook.worksheets[0];
-// Setting value in the cell.
-    sheet.getRangeByName('A1').setText('John');
-    sheet.getRangeByName('A2').setText('Amy');
-    sheet.getRangeByName('A3').setText('Jack');
-    sheet.getRangeByName('A4').setText('Tiya');
-    sheet.getRangeByName('B1').setNumber(10);
-    sheet.getRangeByName('B2').setNumber(12);
-    sheet.getRangeByName('B3').setNumber(20);
-    sheet.getRangeByName('B4').setNumber(21);
-// Create an instances of chart collection.
-    final ChartCollection charts = ChartCollection(sheet);
-// Add the chart.
-    final Chart chart = charts.add();
-// Set Chart Type.
-    chart.chartType = ExcelChartType.column;
-// Set data range in the worksheet.
-    chart.dataRange = sheet.getRangeByName('A1:B4');
-// set charts to worksheet.
-    sheet.charts = charts;
-// save and dispose the workbook.
-    List<int> bytes = workbook.saveAsStream();
-    File('Chart.xlsx').writeAsBytes(bytes);
-    workbook.dispose();
-
-    // //Get the storage folder location using path_provider package.
-    // final Directory directory =
-    // await path_provider.getApplicationDocumentsDirectory();
-    // final String path = directory.path;
-    // final File file = File('$path/output.xlsx');
-    // await file.writeAsBytes(bytes);
-    //
-    // //Launch the file (used open_file package)
-    // await open_file.OpenFile.open('$path/output.xlsx');
   }
 }
